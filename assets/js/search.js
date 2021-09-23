f06267f75c32630240c8f5905761bfbdf0ea809a
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2021-09-22 20:23:39 -0600

    barra de busqueda

---
+++ assets/js/search.js
@@ -0,0 +1,17 @@
+
+function buscarUsuario(){
+    var carnet = document.querySelector('#barraBusqueda').value
+
+    fetch('http://192.168.0.13:3000/buscar/'+carnet,{
+        method: 'GET'
+        })
+        .then(res => res.json())
+        .catch(err =>{
+            console.log("Ha ocurrido un error");
+            alert("Ha ocurrido un error")
+        })
+        .then(response =>{
+            console.log(response)
+            alert(response.data)
+        })
+}
